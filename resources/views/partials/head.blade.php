7d44763069b37832c4d3b003b55dedece31b4f48
Steven Polido <stevendotpulido@gmail.com>
2021-08-13 17:48:28 -0700

    Added og graph tags

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -2,5 +2,11 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <meta prefix="og: http://ogp.me/ns#" property="og:title" content="Live Joe Dreamz | San Diego Hip-Hop artist" />
+  <meta prefix="og: http://ogp.me/ns#" property="og:type" content="website" />
+  <meta prefix="og: http://ogp.me/ns#" property="og:image" content=@php echo bloginfo('url') . "/wp-content/uploads/2021/08/image1.jpeg" @endphp />
+  <meta prefix="og: http://ogp.me/ns#" property="og:url" content="{Your content}" />
+  <meta prefix="og: http://ogp.me/ns#" property="og:description" content="San Diego based Hip-Hop artist." />
   @php wp_head() @endphp
 </head>
