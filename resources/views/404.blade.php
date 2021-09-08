1dd07558b01aa1c83ccefce17c9ea59fcb15f7e1
Steven Polido <stevendotpulido@gmail.com>
2021-09-08 13:49:50 -0700

    Styled a custom 404

--- resources/views/404.blade.php
+++ resources/views/404.blade.php
@@ -1,12 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
-  @include('partials.page-header')
+
 
   @if (!have_posts())
-    <div class="alert alert-warning">
-      {{ __('Sorry, but the page you were trying to view does not exist.', 'sage') }}
+    <div class="404" style="background:url(/wp-content/uploads/2021/08/image1.jpeg); width: 100%; height: 100vh; background-size: cover;">
+      </div>
+      <div style="position: absolute;
+      top: 3rem;
+      left: 0;
+      right: 0;
+      text-align: center;
+      top: 30%;
+      background: rgba(255,255,255, 0.8);
+      padding: 2rem;">
+        <h1 style="font-weight: bold;">404</h1>
+        <p style="font-weight: 100;">The page you requested was not found</p>
+      </div>
     </div>
-    {!! get_search_form(false) !!}
+
   @endif
 @endsection
